e4bfaab7eda3ef03f33829dcff0ca885fd7697b7
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-10-18 11:27:01 +0200

    fix(main): fix typo

--- src/HelloJenkins.java
+++ src/HelloJenkins.java
@@ -3,9 +3,8 @@ public class HelloJenkins {
   public static void main(String[] args) {
     System.out.println(returnWithJenkins());
   }
-  
+
   public static String returnWithJenkins() {
     return "Jenkins";
   }
-
 }
