e4b0c16ee7a5236a1449aed83c912e635be82064
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-10-18 17:23:53 +0200

    chore(app): init app with gitignore file

---
+++ spring-app/src/main/java/com/greenfoxacademy/jenkinsspring/JenkinsSpringApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.jenkinsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSpringApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JenkinsSpringApplication.class, args);
+  }
+}
