1ac085aa90f0443aa426e10362543ff0f6852d3f
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-10-25 13:58:50 +0200

    feat(app): made app bootable

--- spring-app/src/main/java/com/greenfoxacademy/jenkinsspring/MainController.java
+++ spring-app/src/main/java/com/greenfoxacademy/jenkinsspring/MainController.java
@@ -1,11 +1,13 @@
 package com.greenfoxacademy.jenkinsspring;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MainController {
 
+  @GetMapping("")
   public String showIndex() {
-    return "index";
+    return "Hello Jenkins! This is Archie's test app for Jenkins deployment practicing.";
   }
 }
